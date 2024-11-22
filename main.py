24301e933fa7c7551b3fea3b8a56edb19c12271f
Bui Thi Kieu <buithikieu16tclc3@gmail.com>
2024-11-22 12:05:28 +0700

    first commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask in Dockerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr hub!"
+    return "Hello, Flask in Dockerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr hubbbbbbbbbbbbbbbb!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
